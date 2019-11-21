dd0411c74f691a567fd06868e88edcf373b58802
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-11-21 14:07:03 -0800

    Add methods to recursively remove images from filesystem

--- content/admin.py
+++ content/admin.py
@@ -8,14 +8,18 @@ admin.site.register(Content)
 
 class ImageAdmin(admin.ModelAdmin):
     def image_tag(self, obj):
-        return format_html('<img src="{}" width="150px" />'.format(obj.image.url))
+        if obj is None:
+            return "Not available"
+        else:
+            return format_html('<img src="{}" width="150px" />'.format(obj.image.url))
 
     def path(self, obj):
         return settings.HOST_NAME + obj.image.url
 
     image_tag.short_description = 'Preview'
 
-    list_display = ['image_tag',]
-    readonly_fields = ['path', 'image_tag']
+    list_display = ['image_tag','filename',]
+    list_display_links = ['image_tag','filename',]
+    readonly_fields = ['path', 'image_tag',]
 
 admin.site.register(Image, ImageAdmin)
--- content/models.py
+++ content/models.py
@@ -1,4 +1,7 @@
+import os
+
 from django.db import models
+from django.dispatch import receiver
 
 from ckeditor.fields import RichTextField
 
@@ -24,8 +27,43 @@ class Image(models.Model):
     created_at = models.DateTimeField(auto_now=True)
     image = models.ImageField(upload_to='images')
 
+    @property
+    def filename(self):
+        return os.path.basename(self.image.name)
+
     class Meta:
         ordering = ['-created_at']
 
     def __str__(self):
-        return self.image.name
+        return self.filename
+
+
+@receiver(models.signals.post_delete, sender=Image)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+    Deletes image from filesystem
+    when corresponding `Image` object is deleted.
+    """
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
+
+@receiver(models.signals.pre_save, sender=Image)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    """
+    Deletes old image from filesystem
+    when corresponding `Image` object is updated
+    with new file.
+    """
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = Image.objects.get(pk=instance.pk).image
+    except Image.DoesNotExist:
+        return False
+
+    new_file = instance.image
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
