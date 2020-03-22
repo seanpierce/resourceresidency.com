43f45174bf3d1615b2c38fc7baa788e76c04ac5b
Sean Pierce <sumler.sean@gmail.com>
2020-03-21 23:32:46 -0700

    Update link to s1

--- app/components/App.vue
+++ app/components/App.vue
@@ -4,7 +4,7 @@
         <img src="/assets/images/somekindofshow.jpg" alt="" style="width:100%">
         <div id="temp-logos">
             <img src="/assets/images/calexo_logo.png" alt="" style="transform:translateY(-100%)" onclick="window.location.href='https://calexo.co'">
-            <img src="/assets/images/s1_logo.png" alt="" style="width:20%" onclick="window.location.href='https://https://s1portland.com/'">
+            <img src="/assets/images/s1_logo.png" alt="" style="width:20%" onclick="window.location.href='https://s1portland.com/'">
         </div>
     </div>
     <!-- <div v-if="loaded">
