ada7a02af8e041b1670bd92b43de2428b6f46b54
Sean Pierce <sumler.sean@gmail.com>
2020-03-28 14:19:50 -0700

    Update content repository

--- content/repository.py
+++ content/repository.py
@@ -1,4 +1,4 @@
-from .models import Content
+from .models import Content, Event, File
 
 class ContentRepository(object):
     """Access layer for content data."""
@@ -11,3 +11,17 @@ class ContentRepository(object):
             'name': content.name,
             'info': content.info
         }
+
+    @staticmethod
+    def get_events():
+        events = []
+        
+        for event in (Event.objects.order_by('-event_date')):
+            events.append({
+                'id': event.id,
+                'name': event.name,
+                'info': event.info,
+                'files': list(File.objects.filter(event=event).values('event_file')),
+            })
+
+        return events
