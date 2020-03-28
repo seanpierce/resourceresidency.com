fd0585495dcb54d5c96841abde7c8ec9a56a7b36
Sean Pierce <sumler.sean@gmail.com>
2020-03-28 14:20:29 -0700

    Update content admin

--- content/admin.py
+++ content/admin.py
@@ -2,24 +2,13 @@ from django.conf import settings
 from django.contrib import admin
 from django.utils.html import format_html
 
-from .models import Content, Image
+from .models import Content, Event, File
+
+class FileInline(admin.TabularInline):
+    model = File
+
+class EventAdmin(admin.ModelAdmin):
+    inlines = (FileInline,)
 
 admin.site.register(Content)
-
-class ImageAdmin(admin.ModelAdmin):
-    def image_tag(self, obj):
-        if obj is None:
-            return "Not available"
-        else:
-            return format_html('<img src="{}" width="150px" />'.format(obj.image.url))
-
-    def path(self, obj):
-        return settings.HOST_NAME + obj.image.url
-
-    image_tag.short_description = 'Preview'
-
-    list_display = ['image_tag','filename',]
-    list_display_links = ['image_tag','filename',]
-    readonly_fields = ['path', 'image_tag',]
-
-admin.site.register(Image, ImageAdmin)
+admin.site.register(Event, EventAdmin)
